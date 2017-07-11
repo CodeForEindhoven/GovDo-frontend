d611e583b7a11f906344e0567207f029a32780db
alecaro <alessandrocarosso@gmail.com>
2017-07-11 11:21:07 +0200

    Style + js icon switch

--- src/js/gui/components/ProgramDetails.js
+++ src/js/gui/components/ProgramDetails.js
@@ -4,11 +4,20 @@ var ProgramDetails = function(){
 		view: function(vnode){
 			if(viewModels.Hierarchy.getProgram()>0){
 				return m(".programdetails",[
-					m(".programdetails-title", {
+					m(".programdetails-title", 
+					viewModels.Hierarchy.getProgramDetails().name),
+					m("i.material-icons", {
 						onclick: function(){
 							state = !state;
-						}
-					},viewModels.Hierarchy.getProgramDetails().name),
+						},
+						class: state?"state-hidden":""
+					}, "arrow_drop_down"),
+					m("i.material-icons", {
+						onclick: function(){
+							state = !state;
+						},
+						class: state?"":"state-hidden"
+					}, "arrow_drop_up"),
 					m(".programdetails-mission",{
 						class: state?"":"state-hidden"
 					},viewModels.Hierarchy.getProgramDetails().mission),
