75606a02a8e5ba923fe0d7d4106e70c0f51af0b4
alecaro <alessandrocarosso@gmail.com>
2017-07-13 17:27:05 +0200

    style edit icon + editor

--- src/js/gui/components/ProgramDetails.js
+++ src/js/gui/components/ProgramDetails.js
@@ -6,12 +6,16 @@ var ProgramDetails = function(){
 				return m(".programdetails",[
 					m(".programdetails-title", 
 					viewModels.Hierarchy.getProgramDetails().name),
-					m("i.material-icons", {
-						onclick: function(){
-							state = !state;
-						},
-					}, state?"arrow_drop_up":"arrow_drop_down"),
-					
+					m(".icons-header", [
+						m("i.material-icons.edit-button", {
+							class: state?"":"state-hidden"
+						}, "build"),
+						m("i.material-icons", {
+							onclick: function(){
+								state = !state;
+							},
+						}, state?"arrow_drop_up":"arrow_drop_down"),
+					]),
 					m(".programdetails-mission",{
 						class: state?"":"state-hidden"
 					},viewModels.Hierarchy.getProgramDetails().mission),
