10766f3122b161ce9f203a5339a1d7fc933a43a2
alecaro <alessandrocarosso@gmail.com>
2017-09-30 13:36:24 +0200

    Remove

--- src/js/gui/components/editor/EffortEditor.js
+++ src/js/gui/components/editor/EffortEditor.js
@@ -413,7 +413,7 @@ var ConnectionEditor = function(){
 									ptrn.speculativeUnrelate(vm.edit(), parent);
 									vnode.attrs.onchange();
 								}
-							}, m("div", {class:"remove"}, "Verwijderen")) : [],
+							}, "Verwijderen") : [],
 						]);
 					})
 				])
