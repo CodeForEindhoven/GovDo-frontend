60e55b1ba1a1144352c3e5a729f8d2669ed6eb19
Marcel Goethals <marcel.goethals@gmail.com>
2017-10-23 20:49:56 +0200

    More Data

--- src/js/gui/layout/DashboardPage.js
+++ src/js/gui/layout/DashboardPage.js
@@ -23,6 +23,40 @@ var DashboardPage = function(){
 						}),
 
 						m(DashboardDistribution, {
+							series: ptrn("person", function(p){
+								return p("effort task", function(e){
+									return e;
+								}).length;
+							}).reduce(function(o,d){
+								if(!o[d]){
+									o[d]=0;
+								}
+								o[d]+=1;
+								return o;
+							},[]),
+							label: "Verdeling van aantal opgaven over personen",
+							labelx: "opgaven",
+							labely: "personen",
+						}),
+
+						m(DashboardDistribution, {
+							series: ptrn("person", function(p){
+								return p("effort task program", function(e){
+									return e;
+								}).length;
+							}).reduce(function(o,d){
+								if(!o[d]){
+									o[d]=0;
+								}
+								o[d]+=1;
+								return o;
+							},[]),
+							label: "Verdeling van aantal programma's over personen",
+							labelx: "programma's",
+							labely: "personen",
+						}),
+
+						m(DashboardDistribution, {
 							series: ptrn("task", function(p){
 								return p("effort", function(e){
 									return e;
@@ -53,6 +87,40 @@ var DashboardPage = function(){
 							},[]),
 							label: "Verdeling van aantal opgaven over programma's",
 							labelx: "opgaven",
+							labely: "programma's",
+						}),
+
+						m(DashboardDistribution, {
+							series: ptrn("program", function(p){
+								return p("task effort", function(e){
+									return e;
+								}).length;
+							}).reduce(function(o,d){
+								if(!o[d]){
+									o[d]=0;
+								}
+								o[d]+=1;
+								return o;
+							},[]),
+							label: "Verdeling van aantal inspanningen over programma's",
+							labelx: "inspanningen",
+							labely: "programma's",
+						}),
+
+						m(DashboardDistribution, {
+							series: ptrn("program", function(p){
+								return p("task effort person", function(e){
+									return e;
+								}).length;
+							}).reduce(function(o,d){
+								if(!o[d]){
+									o[d]=0;
+								}
+								o[d]+=1;
+								return o;
+							},[]),
+							label: "Verdeling van aantal mensen over programma's",
+							labelx: "mensen",
 							labely: "programma's",
 						})
 					]),
