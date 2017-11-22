881b573461c4e506da092a74c8a2deac01484720
Marcel Goethals <marcel.goethals@gmail.com>
2017-11-22 01:09:39 +0100

    Edit program

--- src/js/gui/components/selector/MissionVision.js
+++ src/js/gui/components/selector/MissionVision.js
@@ -3,9 +3,18 @@ var MissionVision = function(){
 	return {
 		view: function(vnode){
 			return m(".mission",[
-				m(".mission-title", "Missie"),
+				m(".mission-title", [
+					m("span", "Missie"),
+					m("span.mission-edit.button-edit-small",{
+						onclick: function(){
+							vm.edit(vm.program());
+						}
+					}, m(Icon, {name: "edit"})),
+				]),
 				m(".mission-mission body-text", vm.program()("mission").value()),
-			])
+
+
+			]);
 		}
 	};
 };
