cf84202bbb0f894c43e0c4c0ec48873d4ade9683
Marcel Goethals <marcel.goethals@gmail.com>
2017-12-12 17:21:37 +0100

    Click outside dropdowns to close

--- src/js/gui/components/navigation/DropdownNav.js
+++ src/js/gui/components/navigation/DropdownNav.js
@@ -12,120 +12,127 @@ var DropdownNav = function(){
 			}
 
 			return [
-				vnode.attrs.state ? m(".programnav-popup", {},[
+				vnode.attrs.state ? [
+					m("div.dropdown-outside", {
+						onclick: function(){
+							vnode.attrs.onpick();
+						}
+					}),
+					m(".programnav-popup", {}, [
 
-					m(".programnav-searchbar",[
-						m("input",{
-							placeholder: "Zoeken naar ...",
-							value: search,
-							oninput: m.withAttr("value", function(v) {
-								search = v;
+						m(".programnav-searchbar",[
+							m("input",{
+								placeholder: "Zoeken naar ...",
+								value: search,
+								oninput: m.withAttr("value", function(v) {
+									search = v;
 
+								})
+							}),
+						]),
+
+						(page===-1) ? m(".programnav-domain",[
+							ptrn("*"+search, function(result){return result;})
+							.filter(function(result){
+								return (["program", "task", "effort", "person"].indexOf(result.type())>-1);
 							})
-						}),
-					]),
-
-					(page===-1) ? m(".programnav-domain",[
-						ptrn("*"+search, function(result){return result;})
-						.filter(function(result){
-							return (["program", "task", "effort", "person"].indexOf(result.type())>-1);
-						})
-						.sort(function(a,b){
-							var indexA = ["person", "program", "task", "effort"].indexOf(a.type());
-							var indexB = ["person", "program", "task", "effort"].indexOf(b.type());
-							return indexA-indexB;
-						})
-						.map(function(result){
-							//count++;
-							return m(".state-selectable.programnav-searchresult", {
-								class: "searchresult-"+result.type(),
-								onclick: function(){
-									if(result.type()==="program"){
-										vm.program(result);
-										vm.focus(result);
-										vnode.attrs.onpick();
-									}
-									if(result.type()==="task"){
-										vm.program(result("program"));
-										vm.task(result);
-										vm.focus(vm.program());
-										vnode.attrs.onpick();
-									}
-									if(result.type()==="effort"){
-										vm.program(result("task program"));
-										vm.task(result("task"));
-										vm.effort(result);
-										vm.focus(vm.program());
-										vnode.attrs.onpick();
-									}
-
-									if(result.type()==="person"){
-										vm.person(result);
-										vm.focus(result);
-										vnode.attrs.onpick();
-									}
-
-								}
-							},[
-								m(".programnav-searchresult-number", m(Numbering, {whole: true, node: result})),
-								m(".programnav-searchresult-title", result.value()),
-								//m(".programbar-program-mission", program.mission)
-							]);
-						}),
-
-					]) : [],
-
-					(page===0) ? [
-						ptrn("domain", function(domain){
-							return m(".programnav-domain",[
-								m(".programnav-domain-name", domain.value()),
-								domain("program", function(program){
-									//count++;
-									return m(".state-selectable.programnav-program.default", {
-										class: (ptrn.compare(vm.program(),program))?"state-selected":"",
-										onclick: function(){
-											vm.program(program);
-											vm.focus(program);
-											vm.page(0);
+							.sort(function(a,b){
+								var indexA = ["person", "program", "task", "effort"].indexOf(a.type());
+								var indexB = ["person", "program", "task", "effort"].indexOf(b.type());
+								return indexA-indexB;
+							})
+							.map(function(result){
+								//count++;
+								return m(".state-selectable.programnav-searchresult", {
+									class: "searchresult-"+result.type(),
+									onclick: function(){
+										if(result.type()==="program"){
+											vm.program(result);
+											vm.focus(result);
 											vnode.attrs.onpick();
 										}
-									},[
-										m(".programnav-program-number", m(Numbering, {node: program})),
-										m(".programnav-program-title", program.value()),
-										//m(".programbar-program-mission", program.mission)
-									]);
-								}),
-
-							]);
-						}),
-
-						m(".programnav-domain",[
-							m(".programnav-domain-name", "MENSEN"),
-							ptrn("person", function(p){return p;})
-								.sort(function(a,b){
-									var nameA=a.value().toLowerCase(), nameB=b.value().toLowerCase();
-									if(nameA < nameB) return -1;
-									if(nameA > nameB) return 1;
-									return 0;
-								})
-								.map(function(person){
-									return m(".state-selectable.programnav-program", {
-										class: (ptrn.compare(vm.person(),person))?"state-selected":"",
-										onclick: function(){
-											vm.person(person);
-											vm.closeall();
-											vm.focus(person);
+										if(result.type()==="task"){
+											vm.program(result("program"));
+											vm.task(result);
+											vm.focus(vm.program());
 											vnode.attrs.onpick();
 										}
-									},[
-										//m(".programnav-program-number", m(Numbering, {node: program})),
-										m(".programnav-program-title", person.value()),
-									]);
-								})
-						])
-					] : [],
+										if(result.type()==="effort"){
+											vm.program(result("task program"));
+											vm.task(result("task"));
+											vm.effort(result);
+											vm.focus(vm.program());
+											vnode.attrs.onpick();
+										}
 
-				]) : []
+										if(result.type()==="person"){
+											vm.person(result);
+											vm.focus(result);
+											vnode.attrs.onpick();
+										}
+
+									}
+								},[
+									m(".programnav-searchresult-number", m(Numbering, {whole: true, node: result})),
+									m(".programnav-searchresult-title", result.value()),
+									//m(".programbar-program-mission", program.mission)
+								]);
+							}),
+
+						]) : [],
+
+						(page===0) ? [
+							ptrn("domain", function(domain){
+								return m(".programnav-domain",[
+									m(".programnav-domain-name", domain.value()),
+									domain("program", function(program){
+										//count++;
+										return m(".state-selectable.programnav-program.default", {
+											class: (ptrn.compare(vm.program(),program))?"state-selected":"",
+											onclick: function(){
+												vm.program(program);
+												vm.focus(program);
+												vm.page(0);
+												vnode.attrs.onpick();
+											}
+										},[
+											m(".programnav-program-number", m(Numbering, {node: program})),
+											m(".programnav-program-title", program.value()),
+											//m(".programbar-program-mission", program.mission)
+										]);
+									}),
+
+								]);
+							}),
+
+							m(".programnav-domain",[
+								m(".programnav-domain-name", "MENSEN"),
+								ptrn("person", function(p){return p;})
+									.sort(function(a,b){
+										var nameA=a.value().toLowerCase(), nameB=b.value().toLowerCase();
+										if(nameA < nameB) return -1;
+										if(nameA > nameB) return 1;
+										return 0;
+									})
+									.map(function(person){
+										return m(".state-selectable.programnav-program", {
+											class: (ptrn.compare(vm.person(),person))?"state-selected":"",
+											onclick: function(){
+												vm.person(person);
+												vm.closeall();
+												vm.focus(person);
+												vnode.attrs.onpick();
+											}
+										},[
+											//m(".programnav-program-number", m(Numbering, {node: program})),
+											m(".programnav-program-title", person.value()),
+										]);
+									})
+							])
+						] : [],
+
+					])
+				] : []
 			];
 		}
 	};
