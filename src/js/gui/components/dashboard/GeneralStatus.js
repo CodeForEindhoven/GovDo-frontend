40dae78ee1eea9ec0e2266efbcb348da126c7c1a
Marcel Goethals <marcel.goethals@gmail.com>
2018-04-06 15:31:09 +0200

    Matrix view

--- src/js/gui/components/dashboard/GeneralStatus.js
+++ src/js/gui/components/dashboard/GeneralStatus.js
@@ -1,48 +1,21 @@
 var GeneralStatus = function(){
-
-	function getFeedbacks(){
-
-
-		return ptrn("feedbacksession", function(session){
-			var values = ptrn("feedback:"+session.value(), function(feedback){
-				return [
-					feedback("effects").value(),
-					feedback("products").value(),
-					feedback("planning").value(),
-					feedback("stakeholders").value(),
-					feedback("conditions").value(),
-				].reduce(function(mp, e){
-					if(!mp[e]){mp[e]=0;}
-					mp[e]++;
-					return mp;
-				},{});
-			}).reduce(function(mp, e){
-				for(var i in e){
-					if(!mp[i]){mp[i]=0;}
-					mp[i]+=e[i];
-				}
-				return mp;
-			},{0:0,1:0,2:0,"":0});
-
-			return {
-				name: session.value(),
-				totals: (values[0]||0)+(values[1]||0)+(values[2]||0)+(values[""]||0),
-				values: values
-			};
-		});
-	}
-
 	return {
 		view: function(vnode){
 			return m(".dashboard-feedbacks", [
 				ptrn("program", function(program){
 					return m(".dashboard-general-program", [
 						m(".dashboard-general-program-name", program.value()),
-						m(".dashboard-general-tasks", program("task", function(task){
+						m(".dashboard-general-tasks", program("task", function(t){return t;}).sort(function(a,b){
+							return parseInt(a("order").value()) - parseInt(b("order").value());
+						}).map(function(task){
 							return m(".dashboard-general-task",[
 								m(".dashboard-general-task-number",task("order").value()),
-								task("effort", function(effort){
-									return m(".dashboard-general-effort",effort("order").value());
+								task("effort", function(e){return e;}).sort(function(a,b){
+									return parseInt(a("order").value()) - parseInt(b("order").value());
+								}).map(function(effort){
+									return m(".dashboard-general-effort",{
+										class: effort('mode').value()==-1?"mode-sketch":"" + " " + effort('mode').value()==-2?"mode-ready":""
+									},effort("order").value());
 								})
 							]);
 						}))
---
+++ src/js/gui/components/dashboard/MatrixStatus.js
@@ -0,0 +1,39 @@
+var MatrixStatus = function(){
+
+	return {
+		view: function(vnode){
+			return m(".dashboard-feedbacks", [
+				m(".dashboard-general-top", ptrn("program", function(program){
+					return m(".dashboard-general-program-top", [
+						m(".dashboard-general-program-top-name", program.value())
+					]);
+				})),
+				m(".dashboard-general-bottom", ptrn("program", function(program){
+					return m(".dashboard-general-program", [
+						m(".dashboard-general-program-name", program.value()),
+						m(".dashboard-general-tasks", program("task", function(t){return t;}).sort(function(a,b){
+							return parseInt(a("order").value()) - parseInt(b("order").value());
+						}).map(function(task){
+							return m(".dashboard-matrix-task",[
+								m(".dashboard-matrix-task-number",task("order").value()),
+								ptrn("program", function(other){
+									return m(".dashboard-matrix-task-program", [
+										task("related effort", function(e){return e;}).filter(function(e){
+											return ptrn.compare(other, e("task program"));
+										}).map(function(effort){
+											return m(".dashboard-matrix-task-program-effort", effort("order").value());
+										})
+									]);
+								})
+								//task("related effort", function(effort){
+									//return m(".dashboard-general-effort",{}, effort.value());
+									//return effort;
+								//})
+							]);
+						}))
+					]);
+				}))
+			]);
+		}
+	};
+};
